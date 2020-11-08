6f59f701a767c386ba702af8fc84b4d75cb99795
willakuffo <william.hawkins71@gmail.com>
2020-11-08 20:06:59 +0000

    corrected threshold and error

--- denoising.m
+++ denoising.m
@@ -68,3 +68,67 @@ ylim([-1 0.7])
 % 
 % hold on
 % plot(origSignal,'k')
+clear;clc;
+data = importdata("denoising_codeChallenge.mat");
+
+origSignal = data.origSignal;
+cleanedSignal = data.cleanedSignal;
+
+figure;
+
+plot(origSignal, 'b')
+
+k = 150;
+% Remove spike noise using the median filter
+upper_threshold = 10;
+lower_threshold = -10;
+
+% initialise median filtered signal
+medianSignal = origSignal;
+
+% get values above the threshold
+aboveThresh = find(medianSignal>upper_threshold);
+
+belowThresh = find(medianSignal<lower_threshold);
+
+for i=1:length(aboveThresh)
+    lowerBound = max(1, aboveThresh(i)-k);
+    upperBound = min(aboveThresh(i)+ k, length(medianSignal));
+    
+    % calculate median of the surrounding points
+    medianSignal(aboveThresh(i)) = 
+median(medianSignal(lowerBound:upperBound));
+    
+end
+
+
+for i=1:length(aboveThresh)
+    lowerBound = max(1, belowThresh(i)-k);
+    upperBound = min(aboveThresh(i)+ k, length(medianSignal));
+    
+    % calculate median of the surrounding points
+    medianSignal(belowThresh(i)) = 
+median(medianSignal(lowerBound:upperBound));
+    
+end
+figure;
+plot(medianSignal,'g')
+%ylim([-3 3])
+
+
+
+% plot(cleanedSignal)
+
+
+% Running mean time series filter
+coefficient = ((2*k+1)^-1);
+meanSignal = medianSignal;
+for i=k+1:length(origSignal)-k-1
+    meanSignal(i) = mean(meanSignal(i-k: i+k));
+end
+
+% Setting the edges to zero
+
+figure;
+plot(meanSignal,'r')
+ylim([-1 1])
